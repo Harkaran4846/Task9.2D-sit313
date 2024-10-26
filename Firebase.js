6d5906a9f54cef6133d24f3f151dc7047be081af
Harkaran4846 <harkaran4846.be23@chitkara.edu.in>
2024-10-26 16:17:37 +0530

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBQwQiqu_sQ7-Kw25e-oPwsXhtrAzwEfdM",
   authDomain: "task-8d-12a23.firebaseapp.com",
@@ -13,11 +11,8 @@ const firebaseConfig = {
   appId: "1:585489430604:web:172a1558423b7291b15ce9"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore and Storage
 export const db = getFirestore(app);
-
 export const storage = getStorage(app);
 
